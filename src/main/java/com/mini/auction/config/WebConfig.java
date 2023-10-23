4a7f27fb33d0bd4ce62b68db6f15ac834f79833d
msk102948 <msk102948@gmail.com>
2023-10-23 21:32:21 +0900

    Interceptor을 통한 JWT 검증 config 설정

---
+++ src/main/java/com/mini/auction/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.mini.auction.config;
+
+import com.mini.auction.config.interceptor.JwtInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer{
+
+    private static final String[] EXCLUDE_PATHS = {
+            /*
+             *  TODO: 토큰이 필요하지 않은 uri에 대해서만 예외처리 필요함
+             */
+            "/**"
+    };
+
+    private final JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(EXCLUDE_PATHS);
+    }
+
+
+}
---
+++ src/main/java/com/mini/auction/config/interceptor/JwtInterceptor.java
@@ -0,0 +1,34 @@
+package com.mini.auction.config.interceptor;
+
+import com.mini.auction.config.jwt.JwtService;
+import com.mini.auction.config.jwt.UnauthorizedException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private static final String HEADER_AUTH = "Authorization";
+
+    private final JwtService jwtService;
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler){
+
+        final String token = request.getHeader(HEADER_AUTH);
+
+        if (token != null && jwtService.isUsable(token)) {
+            return true;
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+}
