6ef705efff67ce7368cadcd15cba5590eb6448b9
msk102948 <msk102948@gmail.com>
2023-11-10 00:17:36 +0900

    로깅 추가

--- src/main/java/com/mini/auction/member/adapter/out/persistence/MemberPersistenceAdapter.java
+++ src/main/java/com/mini/auction/member/adapter/out/persistence/MemberPersistenceAdapter.java
@@ -2,6 +2,7 @@ package com.mini.auction.member.adapter.out.persistence;
 
 import com.mini.auction.common.exceptionHandler.CustomResponse;
 import com.mini.auction.common.exceptionHandler.ExceptionCode;
+import com.mini.auction.common.exceptionHandler.GlobalExceptionHandler;
 import com.mini.auction.common.exceptionHandler.customException.BadRequestException;
 import com.mini.auction.common.exceptionHandler.customException.NotFoundAuthException;
 import com.mini.auction.config.jwt.JwtToken;
@@ -10,6 +11,8 @@ import com.mini.auction.member.adapter.in.web.dto.RegistrationInfoReq;
 import com.mini.auction.member.application.port.out.AccountPort;
 import com.mini.auction.member.application.port.out.FindMemberPort;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +25,8 @@ class MemberPersistenceAdapter implements AccountPort, FindMemberPort {
     private final MemberRepository memberRepository;
     private final MemberEntityMapper memberEntityMapper;
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @Override
     public boolean existById(String id) {
         return memberRepository.existsById(id);
@@ -33,6 +38,8 @@ class MemberPersistenceAdapter implements AccountPort, FindMemberPort {
         try {
             memberRepository.save(register);
         } catch (DataIntegrityViolationException e) { // sql 오류나 입력된 data가 잘못된 경우 동작하는 exception
+            logger.info("error class: {}", e.getClass());
+            logger.info("error message: {}", e.getMessage());
             throw new BadRequestException(new CustomResponse(ExceptionCode.E10002));
         }
     }
